7591c4b38f2f0e39d1527ba79b83815e534f5b20
WolfieZero <neil@wolfiezero.com>
2016-04-29 11:35:59 +0100

    added browserify support

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,29 @@
+// =============================================================================
+// Gulpfile
+// =============================================================================
 
+
+var assign      = require('lodash.assign');
+var browserify  = require('browserify');
 var browserSync = require('browser-sync').create();
+var buffer      = require('vinyl-buffer');
 var gulp        = require('gulp');
+var gutil       = require('gulp-util');
 var sass        = require('gulp-sass');
+var source      = require('vinyl-source-stream');
 var sourcemaps  = require('gulp-sourcemaps');
+var watchify    = require('watchify');
 
+var watching = false;
+var opts = assign({}, watchify.args, {
+    entries: ['./app/js/app.js'],
+    debug: true
+});
+var ify  = browserify(opts);
+
+
+// Browser Sync
+// =============================================================================
 
 gulp.task('browser-sync', function() {
     browserSync.init({
@@ -17,6 +37,9 @@ gulp.task('browser-sync', function() {
 });
 
 
+// Sass
+// =============================================================================
+
 gulp.task('sass', function() {
     return gulp.src('./app/sass/**/*.scss')
         .pipe(sourcemaps.init())
@@ -27,13 +50,50 @@ gulp.task('sass', function() {
 });
 
 
-gulp.task('serve', function() {
+// Browserify
+// =============================================================================
+
+gulp.task('browserify', bundle);
+
+if (watching) {
+    ify = watchify(ify());
+}
+
+ify.on('update', bundle); // on any dep update, runs the bundler
+ify.on('log', gutil.log); // output build logs to terminal
+
+function bundle() {
+    return ify.bundle()
+        .on('error', gutil.log.bind(gutil, 'Browserify Error'))
+        .pipe(source('bundle.js'))
+        .pipe(buffer())
+        .pipe(sourcemaps.init())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('./app/js/'))
+        .pipe(browserSync.stream());
+}
+
+
+// Serve
+// =============================================================================
+
+gulp.task('serve', ['build'], function() {
     browserSync.init({
         server: './app'
     });
     gulp.watch('./app/sass/**/*.scss', ['sass']);
+    gulp.watch('./app/js/**/*.js', ['browserify']);
     gulp.watch('./app/**/*.html').on('change', browserSync.reload);
 });
 
+
+// Serve
+// =============================================================================
+
+gulp.task('build', ['sass', 'browserify']);
+
+
+// Default
+// =============================================================================
 
 gulp.task('default', ['serve']);
